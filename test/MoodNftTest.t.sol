85667313ff0c33ab0b4aa79c119b8866fa036650
Nwachee <fuzzy245.in@gmail.com>
2025-08-01 23:23:25 +0100

    Test : added test for SVG

---
+++ test/MoodNftTest.t.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract MoodNftTest is Test {
+    MoodNft moodNft;
+
+    string public constant SAD_SVG_URI = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj4KPHBhdGggZD0iTTEwIDVIMVYxMEg1VjV6IiBmaWxsPSJibGFjayIvPgo8L3N2Zz4=";
+    string public constant HAPPY_SVG_URI = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj4KPHBhdGggZD0iTTEwIDVIMVYxMEg1VjV6IiBmaWxsPSJ5ZWxsb3ciLz4KPC9zdmc+Cg==";
+
+    address USER = makeAddr("USER");
+
+
+    function setUp() public {
+        moodNft = new MoodNft(SAD_SVG_URI, HAPPY_SVG_URI);
+    }
+
+    function testViewTokenURI() public {
+        vm.prank(USER);
+        moodNft.mintNft();
+        console.log(moodNft.tokenURI(0));
+    }
+}
